2a8387d931f32d5169ce9c82a601424efb027519
shin820 <shin820@163.com>
2013-06-21 18:26:33 +0800

    change reference

--- ProgrammingEF/Chapter2ConsoleApp/Program.cs
+++ ProgrammingEF/Chapter2ConsoleApp/Program.cs
@@ -9,6 +9,22 @@ namespace Chapter2ConsoleApp
     {
         static void Main(string[] args)
         {
+            QueryContacts();
+        }
+
+        private static void QueryContacts()
+        {
+            using (var context = new SampleEntities())
+            {
+                var contacts = from c in context.Contacts select c;
+
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                }
+
+                Console.Read();
+            }
         }
     }
 }
